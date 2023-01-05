47c4f85c3be373352854662716e58d2b368175a5
MohamedAbdallah-14 <mohamedmabdallah14@gmail.com>
2023-01-05 14:23:03 +0200

    Initial commit

---
+++ bin/blind75.dart
@@ -0,0 +1,5 @@
+import 'package:blind75/blind75.dart' as blind75;
+
+void main(List<String> arguments) {
+  print('Hello world: ${blind75.calculate()}!');
+}
---
+++ test/blind75_test.dart
@@ -0,0 +1,8 @@
+import 'package:blind75/blind75.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
